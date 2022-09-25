7d3a3ed278bd7699d3532b4cbbab35315e67fb9c
Corey Cole <3700879+corey-cole@users.noreply.github.com>
2022-09-25 14:20:28 -0700

    Use Terraform binary directly for better CI test results

--- examples/terraform-basic-example/main.tf
+++ examples/terraform-basic-example/main.tf
@@ -1,7 +1,7 @@
 module "lambda_insights_layer" {
-    // Relative path for testing.  Use commented line to point to GitHub version
-    source = "../../"
-    //source = "github.com/corey-cole/terraform-lambda-insights-lookup"
+  // Relative path for testing.  Use commented line to point to GitHub version
+  source = "../../"
+  //source = "github.com/corey-cole/terraform-lambda-insights-lookup"
 
-    region = var.region
+  region = var.region
 }
